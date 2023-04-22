1db6e66260fd376dc7f0587fe96e3e9b76ee85c2
jirulak02 <tchooorulok@gmail.com>
2023-04-22 22:04:39 +0200

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/layout/Header";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export type Children = {
 	children: React.ReactNode;
@@ -12,10 +12,8 @@ export type Children = {
 
 export default function RootLayout({ children }: Children) {
 	return (
-		<html lang="en">
-			<body
-				className={`${inter.className} flex flex-col min-h-screen relative`}
-			>
+		<html lang="en" className={`${inter.variable} font-sans`}>
+			<body className="flex flex-col min-h-screen relative">
 				<Header normal />
 				<main className="pt-16">{children}</main>
 				<Footer />
