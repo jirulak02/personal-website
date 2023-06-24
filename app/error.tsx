8ea06f983e237bb8dd3398cdaf5de337ff6a368b
Jiri Simecek <jirulak02@gmail.com>
2023-06-24 16:37:41 +0200

    center properly

--- app/error.tsx
+++ app/error.tsx
@@ -2,7 +2,7 @@
 
 function Error({ error, reset }: { error: Error; reset: () => void }) {
 	return (
-		<div className="text-center absolute top-1/3 w-full flex flex-col space-y-3 p-3">
+		<div className="text-center absolute top-1/2 -translate-y-1/2 w-full flex flex-col space-y-3 p-3">
 			<h2 className="font-semibold">Something went wrong!</h2>
 			<button
 				className="m-auto p-4 text-lg sm:text-xl md:text-2xl py-2 rounded-3xl bg-lime-800 text-neutral-100 border-2 border-lime-800 hover:bg-neutral-100 hover:text-lime-800 focus:bg-neutral-300"
