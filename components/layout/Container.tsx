2fe546d22bf27d7706859f1595c038f77abaa1fd
jirulak02 <jirulak02@gmail.com>
2025-11-11 02:44:48 +0700

    feat: simple html

--- components/layout/Container.tsx
+++
@@ -1,3 +0,0 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="m-auto max-w-6xl grow flex flex-col justify-between px-3">{children}</div>;
-}
